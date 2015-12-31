423e4794c4807ca175a8b41facc964a46d2e006e
Pierre <frin@et.esiea.fr>
2015-12-31 11:52:31 +0100

    uploader image

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -2,7 +2,8 @@ class Sport < ActiveRecord::Base
 	has_many :equipements, inverse_of: :sport
 
 	has_attached_file :image, styles: {small: "64x64", med: "100x100", large: "200x200"}
-
+	
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates :name, :description, presence:  true
 	validates :name, length: {minimum: 3}
 	validates :name, uniqueness: { case_sensitive: false }
